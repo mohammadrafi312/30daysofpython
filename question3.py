9d546d1e348e26d73d5257c37b026a84b66af9d3
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-11 23:54:55 +0530

    added question 3

---
+++ question3.py
@@ -0,0 +1,55 @@
+# Q18 — Hardest Coding Challenge of Day 1
+
+# Build a Student Result Calculator.
+
+# Take:
+
+# Student name
+# Age
+# Subject 1 marks
+# Subject 2 marks
+# Subject 3 marks
+
+# Then display:
+
+# ========== RESULT ==========
+
+# Name       : Rafi
+# Age        : 20
+
+# Subject 1  : 87
+# Subject 2  : 92
+# Subject 3  : 78
+
+# Total      : 257
+# Average    : 85.6666666667
+# Percentage : 85.6666666667%
+# ============================
+# Restrictions
+
+# You can use:
+
+# ✅ Variables
+# ✅ input()
+# ✅ print()
+# ✅ int() / float()
+# ✅ Arithmetic operators
+# ✅ f-strings
+
+# You cannot use:
+
+# ❌ if
+# ❌ else
+# ❌ elif
+# ❌ loops
+# ❌ lists
+# ❌ dictionaries
+# ❌ functions
+
+# 💡 Hint
+
+# You'll need:
+
+# total = mark1 + mark2 + mark3
+# average = total / 3
+# percentage = ...
