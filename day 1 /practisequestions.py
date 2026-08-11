faea2a153b2be8342376be26acedaef1a14a93d4
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-11 23:44:39 +0530

    added comments

--- day 1 /practisequestions.py
+++ day 1 /practisequestions.py
@@ -1,3 +1,33 @@
+# Write a Python program that takes one integer from the user and prints:
+
+# Number: 25
+# Double: 50
+# Triple: 75
+# Square: 625
+# Half: 12.5
+# Remainder when divided by 3: 1
+# Restrictions
+
+# You can use:
+
+# ✅ input()
+# ✅ print()
+# ✅ Variables
+# ✅ int() / float()
+# ✅ Arithmetic operators
+
+# You cannot use:
+
+# ❌ if
+# ❌ else
+# ❌ loops
+# ❌ lists
+# ❌ functions
+
+
+
+
+
 n =int(input("enter the number:"))
 print("number :",n)
 print("double :",n*2)
