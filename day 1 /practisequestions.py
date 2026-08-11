026251d268cdafab825bdefd24b5f2465f28567f
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-11 23:40:25 +0530

    added day1 prctise q1

---
+++ day 1 /practisequestions.py
@@ -0,0 +1,8 @@
+n =int(input("enter the number:"))
+print("number :",n)
+print("double :",n*2)
+print("Triple :",n+n+n)
+print("square :",n**2)
+print("half:",n/2)
+print("remainder when divided by 3 :",n%3)
+
