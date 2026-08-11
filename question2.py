27cfe319d49ad643f4e3a6969f3f81e948f4e9ea
SHAIK MOHAMMAD RAFI <shaik.mohammadrafi2712@gmail.com>
2026-08-11 23:54:01 +0530

    added q1

---
+++ question2.py
@@ -0,0 +1,36 @@
+# Write a program that takes:
+
+# Name
+# Age
+
+# and produces:
+
+# My name is Rafi.
+# I am 20 years old.
+# After 10 years, I will be 30.
+# Restrictions
+
+# Use only:
+
+# ✅ input()
+# ✅ print()
+# ✅ Variables
+# ✅ int() / str()
+# ✅ Arithmetic
+# ✅ f-strings if you want
+
+# Don't use:
+
+# ❌ if
+# ❌ loops
+# ❌ lists
+# ❌ functions
+
+
+# code :
+
+name =input("enter your name :")
+age =int(input("enter your age :"))
+print("My name is",name)
+print("I am",age,"Years old..")
+print("after 10 Years , I will be",(10+age))
